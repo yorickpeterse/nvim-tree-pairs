fc955631eb2d230b390430fa25c87ae68af49192
Yorick Peterse <git@yorickpeterse.com>
2024-04-18 15:56:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# nvim-tree-pairs
+
+A NeoVim plugin that uses Tree-sitter to allow jumping between the matching
+opposing end of a Tree-sitter node, such as brackets, quotes, and more.
+
+[![A recording of using the plugin](https://asciinema.org/a/654974.svg)](https://asciinema.org/a/654974)
+
+# Why?
+
+When using Tree-sitter, the `%` motion of NeoVim doesn't always work reliably,
+as it requires syntax information not present when using Tree-sitter. This is
+most notable when you're jumping between matching brackets (e.g. `{` and `}`),
+but there are a lot of lines in between, as in this case `%` might jump to some
+random location in the middle.
+
+This plugin solves this issue by using Tree-sitter when enabled, falling back to
+the default `%` behaviour if Tree-sitter isn't enabled. When Tree-sitter is
+available, you can jump between the start and end of any Tree-sitter node. Take
+this Lua code for example:
+
+```lua
+example
+```
+
+When you place the cursor on the `e` in `example` and press `%`, the cursor
+jumps to the closing `e`. If you then press `%` again, it jumps back to the
+starting `e`.
+
+# Requirements
+
+- NeoVim `main`, 0.9.5 _might_ also work but I haven't verified this
+- [nvim-treesitter](https://github.com/nvim-treesitter/nvim-treesitter/)
+
+# Installation
+
+Add `yorickpeterse/nvim-tree-pairs` using your favourite package manager, then
+add the following somewhere to your `init.lua`:
+
+```lua
+require('tree-pairs').setup()
+```
+
+This creates a "pairs" Tree-sitter module that's enabled automatically. You can
+disable it (if needed) using `:TSDisable pairs`.
+
+# Limitations
+
+This plugin doesn't have any language specific handling or Tree-sitter queries,
+meaning it can't handle jumping from e.g. the Lua `then` keyword to the next
+`elseif` keyword. This is by design, as the intent of this plugin is to be as
+simple as possible. Patches that apply language specific handling won't be
+accepted.
+
+# License
+
+All source code in this repository is licensed under the Mozilla Public License
+version 2.0, unless stated otherwise. A copy of this license can be found in the
+file "LICENSE".
---
+++ lua/tree-pairs/init.lua
@@ -0,0 +1,84 @@
+local api = vim.api
+local ts = vim.treesitter
+local utils = require('nvim-treesitter.ts_utils')
+local parsers = require('nvim-treesitter.parsers')
+local M = {}
+
+local MODES = {
+  n = "call matchit#Match_wrapper('',1,'n')",
+  x = "call matchit#Match_wrapper('',1,'v')",
+  o = "call matchit#Match_wrapper('',1,'o')",
+}
+
+local function match(fallback)
+  if not parsers.has_parser() then
+    return vim.cmd(fallback)
+  end
+
+  local root = ts.get_node()
+
+  if not root then
+    return
+  end
+
+  local head = root:child(0)
+  local tail = root:child(root:child_count() - 1)
+  local pos = api.nvim_win_get_cursor(0)
+  local cursor_row = pos[1] - 1
+  local cursor_col = pos[2]
+
+  -- The head and the tail might be the same for certain nodes (e.g. Rust
+  -- booleans), in which case we use the fallback approach so you can easily
+  -- jump between the start and the end of such nodes.
+  if head and tail and not head:equal(tail) then
+    -- This approach allows you to e.g. jump from an "if" to a matching "end",
+    -- regardless of whether the cursor is on the "i" or "f".
+    if ts.is_in_node_range(tail, cursor_row, cursor_col) then
+      utils.goto_node(head, false)
+    else
+      utils.goto_node(tail, true)
+    end
+  else
+    -- This approach checks if the cursor is at exactly the first or last
+    -- character in the node. This is used as a fallback for nodes without
+    -- children, such as a boolean literal. This way if the cursor is at "t" in
+    -- "true", you can jump to the "e" and the other way around.
+    local start_row, start_col, _ = root:start()
+    local end_row, end_col, _ = root:end_()
+
+    if cursor_row == end_row and cursor_col == end_col - 1 then
+      api.nvim_win_set_cursor(0, { start_row + 1, start_col })
+    else
+      -- End columns point to a position _after_ the end of the node, so we have
+      -- to subtract 1.
+      api.nvim_win_set_cursor(0, { end_row + 1, end_col - 1 })
+    end
+  end
+end
+
+function M.setup()
+  require('nvim-treesitter').define_modules({
+    pairs = {
+      enable = true,
+      attach = function(buf)
+        local opts = {
+          desc = 'Jump to the opposite end of the current Tree-sitter node',
+          buffer = buf,
+        }
+
+        for mode, fallback in pairs(MODES) do
+          vim.keymap.set(mode, '%', function()
+            match(fallback)
+          end, opts)
+        end
+      end,
+      detach = function(buf)
+        for mode, _ in pairs(MODES) do
+          vim.keymap.del(mode, '%', { buffer = buf })
+        end
+      end,
+    },
+  })
+end
+
+return M
