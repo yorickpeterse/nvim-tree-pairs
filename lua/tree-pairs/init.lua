1ac9387d4d36b88a319c661d93b95e8414f73e2f
Yorick Peterse <git@yorickpeterse.com>
2024-04-19 21:54:01 +0200

    Improve fallback logic
    This allows handling of pairs in comments and string literals, and
    improves the handling of parsers not being available.
    
    This fixes https://github.com/yorickpeterse/nvim-tree-pairs/issues/2.

--- lua/tree-pairs/init.lua
+++ lua/tree-pairs/init.lua
@@ -2,8 +2,44 @@ local api = vim.api
 local ts = vim.treesitter
 local parsers = require('nvim-treesitter.parsers')
 local M = {}
+local AUGROUP = api.nvim_create_augroup('tree-pairs', {})
 
-local MODES = { 'n', 'x', 'o' }
+-- The modes for which to enable the mapping, along with their fallback
+-- strategies.
+local MODES = {
+  n = function()
+    vim.fn['matchit#Match_wrapper']('', 1, 'n')
+  end,
+  x = function()
+    vim.fn['matchit#Match_wrapper']('', 1, 'v')
+  end,
+  o = function()
+    vim.fn['matchit#Match_wrapper']('', 1, 'o')
+  end,
+}
+
+-- The node types for which to fall back to using matchit.
+local FALLBACK = {
+  block_comment = true,
+  line_comment = true,
+  string_content = true,
+  string_fragment = true,
+}
+
+-- The symbols to check for when falling back to using matchit.
+local PAIRS = {
+  ['['] = true,
+  ['('] = true,
+  ['{'] = true,
+  ['<'] = true,
+  [']'] = true,
+  [')'] = true,
+  ['}'] = true,
+  ['>'] = true,
+  ['"'] = true,
+  ["'"] = true,
+  ['`'] = true,
+}
 
 local function jump_to_node(node, jump_to_end)
   local start_row, start_col, end_row, end_col = node:range(false)
@@ -21,18 +57,61 @@ local function jump_to_node(node, jump_to_end)
   end
 end
 
-local function match()
-  local root = ts.get_node()
+local function char_under_cursor()
+  local pos = api.nvim_win_get_cursor(0)
+  local row = pos[1] - 1
+  local col = pos[2]
 
-  if not root then
+  return api.nvim_buf_get_text(0, row, col, row, col + 1, {})[1]
+end
+
+local function match(buf, fallback)
+  local has_parser, _ = pcall(vim.treesitter.get_parser, buf)
+
+  if not has_parser then
+    if PAIRS[char_under_cursor()] then
+      fallback()
+    end
+
     return
   end
 
-  local head = root:child(0)
-  local tail = root:child(root:child_count() - 1)
+  local root = ts.get_node()
+
+  if not root then
+    if PAIRS[char_under_cursor()] then
+      fallback()
+    end
+
+    return
+  end
+
+  if FALLBACK[root:type()] then
+    if PAIRS[char_under_cursor()] then
+      return fallback()
+    end
+  end
+
   local pos = api.nvim_win_get_cursor(0)
   local cursor_row = pos[1] - 1
   local cursor_col = pos[2]
+  local head = root:child(0)
+  local tail = root:child(root:child_count() - 1)
+
+  -- For certain languages, start/end characters (e.g. the
+  -- quotes in a string) are separate nodes inside a parent node. An example is
+  -- Python, which parses strings as
+  -- `(string (string_start) (string_content) (string_end))`. This approach
+  -- tries to handle such cases, without the need for Tree-sitter queries.
+  if not head and not tail and PAIRS[char_under_cursor()] then
+    local parent = root:parent()
+
+    if parent then
+      root = parent
+      head = parent:child(0)
+      tail = parent:child(root:child_count() - 1)
+    end
+  end
 
   -- The head and the tail might be the same for certain nodes (e.g. Rust
   -- booleans), in which case we use the fallback approach so you can easily
@@ -64,25 +143,23 @@ local function match()
 end
 
 function M.setup()
-  require('nvim-treesitter').define_modules({
-    pairs = {
-      enable = true,
-      attach = function(buf)
-        local opts = {
-          desc = 'Jump to the opposite end of the current Tree-sitter node',
-          buffer = buf,
-        }
+  api.nvim_create_autocmd('FileType', {
+    group = AUGROUP,
+    pattern = '*',
+    desc = 'Sets up tree-pairs for a buffer',
+    callback = function()
+      local buf = api.nvim_get_current_buf()
+      local opts = {
+        desc = 'Jump to the opposite end of the current Tree-sitter node',
+        buffer = buf,
+      }
 
-        for _, mode in ipairs(MODES) do
-          vim.keymap.set(mode, '%', match, opts)
-        end
-      end,
-      detach = function(buf)
-        for _, mode in ipairs(MODES) do
-          vim.keymap.del(mode, '%', { buffer = buf })
-        end
-      end,
-    },
+      for mode, fallback in pairs(MODES) do
+        vim.keymap.set(mode, '%', function()
+          match(buf, fallback)
+        end, opts)
+      end
+    end,
   })
 end
 
